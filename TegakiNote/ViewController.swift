ee8b155420754071750aa1d31716ee1b9f255ea1
msaka73 <msaka73@gmail.com>
2020-07-14 06:04:26 +0900

    Initial Commit

---
+++ TegakiNote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TegakiNote
+//
+//  Created by 坂本充生 on 2020/07/14.
+//  Copyright © 2020 Michio.Sakamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
