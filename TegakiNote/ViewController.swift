6d387958690300a201ded1aa662699bfc182042b
msaka73 <msaka73@gmail.com>
2020-07-14 12:55:12 +0900

    2020/7/14 12:55

--- TegakiNote/ViewController.swift
+++ TegakiNote/ViewController.swift
@@ -22,21 +22,20 @@ class ViewController: UIViewController {
         
         let unitWidth = view.frame.width / 10         //幅の1/10
         let unitHeight = view.frame.height / 10       //高さの1/10
-        let unitMultiple:CGFloat = 9.0        //何倍にするか
+        let unitMultiple:CGFloat = 8.7        //何倍にするか
         
 //        let canvas = PKCanvasView(frame: view.frame)
-//        canvas = PKCanvasView(frame: CGRect(x: unitWidth , y: unitHeight, width: unitWidth * unitMultiple, height: unitHeight * unitMultiple))
+//        let canvas = PKCanvasView(frame: CGRect(x: unitWidth , y: unitHeight, width: unitWidth * unitMultiple, height: unitHeight * unitMultiple))
         canvas.frame = CGRect(x: unitWidth , y: unitHeight, width: unitWidth * unitMultiple, height: unitHeight * unitMultiple)
         canvas.isOpaque = false
-        canvas.backgroundColor = UIColor.init(red: 1, green: 1, blue: 1, alpha: 1)
-        canvas.layer.borderWidth = 5
-//        canvas.layer.borderColor = UIColor.black
-        canvas.layer.borderColor = CGColor.init(srgbRed: 0, green: 0, blue: 0, alpha: 1)
+        canvas.backgroundColor = UIColor.init(red: 1, green: 1, blue: 0.93, alpha: 1)      //canvas背景色
+        canvas.layer.borderWidth = 5        //枠線幅
+        canvas.layer.borderColor = CGColor.init(srgbRed: 1, green: 0, blue: 0, alpha: 1)    //枠線色
         view.addSubview(canvas)
         
         //
 //        view.isOpaque = false
-//        view.backgroundColor = UIColor.init(red: 1, green: 1, blue: 1, alpha: 1)
+        view.backgroundColor = UIColor.init(red: 1, green: 1, blue: 1, alpha: 1)    //UIViewの背景色
         
         canvas.tool = PKInkingTool(.pencil, color: .blue ,width:  30)
         // PKToolPicker: ドラッグして移動できるツールパレット (ペンや色などを選択できるツール)
@@ -44,6 +43,7 @@ class ViewController: UIViewController {
             if let toolPicker = PKToolPicker.shared(for: window) {
                 toolPicker.addObserver(canvas)
                 toolPicker.setVisible(true, forFirstResponder: canvas)
+                
                 canvas.becomeFirstResponder()
             }
         }
